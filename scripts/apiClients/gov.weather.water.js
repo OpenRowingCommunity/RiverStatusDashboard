43a1dc1e9e4ed737137e5fc89d1ef76b80247122
Adrian Edwards <adrian@adriancedwards.com>
2023-09-01 19:40:56 -0400

    refactor weather.gov water API to use new structure

--- scripts/apiClients/gov.weather.water.js
+++ scripts/apiClients/gov.weather.water.js
@@ -4,103 +4,73 @@
 //		gov.weather.js created on 2017-06-26
 
 
-var gov_weather_water = {
-	
-	cache: {
-		data: null,
-		timestamp: null,
-		agelimit: null
-	},
-	
-	api: {
-		url: 'https://water.weather.gov/ahps2/hydrograph_to_xml.php',
-		params: {
-			gage: config.getDataSourceDetailsByType(APIClientIdentifier.NOAA_WATER)[0].id,
-			output: 'xml'		// switch to JSON? -- unsupported yet
-		}
-	},
-	
-	// helper functions
-	unitsFormatter: function (measure, units_i) {
-		return units_i;
-	},
-	
-	valueFormatter: function (measure, value_i) {
-		return value_i;
-	},
-	
-	// api functions
-	getWaterFlow: function (setterFunc) {
-		//	1: check if we have data already
-		if (gov_weather_water.cache.data != null) {
-			//	2: if the timestamp is acceptably recent, use it
-			if ( (now - cache.timestamp) < agelimit ){
-				let value = cache.data.THEVALUE;			//TODO: replace with real value
-				setterFunc(value);
-				return;
-			}
-		}
-		
-		let asyncContext = gov_weather_water;
-		
-		$.ajax({
-			url: asyncContext.api.url, 
-			data: asyncContext.api.params, 
-			datatype: asyncContext.api.params.format,
-			success: function (data, textStatus, jqXHR) {
-				
-				// get from data -- XPaths?
-				var datum = $(data).find('observed > datum:first');
-				var flowUnits = $(datum).find('secondary').attr('units');
-				var flowValue = $(datum).find('secondary').text();
-				flowUnits = asyncContext.unitsFormatter('flow', flowUnits);
-				flowValue = asyncContext.valueFormatter('flow', flowValue);
-				
-				let apiData = "" + flowValue /*+ " " + flowUnits*/;
-				
-				// TODO: update the cached data
-				setterFunc(apiData);
-			}//end-success
-		});//end-$.ajax
-		
-	},//end-getWaterFlow
-	
-	getWaterLevel: function (setterFunc) {
-		
-		//	1: check if we have data already
-		if (gov_weather_water.cache.data != null) {
-			//	2: if the timestamp is acceptably recent, use it
-			if ( (now - cache.timestamp) < agelimit ){
-				let value = cache.data.THEVALUE;			//TODO: replace with real value
-				setterFunc(value);
-				return;
-			}
-		}
-		
-		let asyncContext = gov_weather_water;
-		
-		$.ajax({
-			url: asyncContext.api.url, 
-			data: asyncContext.api.params, 
-			datatype: asyncContext.api.params.format,
-			success: function (data, textStatus, jqXHR) {
-				
-				// get from data
-				var datum = $(data).find('observed > datum:first');
-				var floodUnits = $(datum).find('primary').attr('units');
-				var floodValue = $(datum).find('primary').text();
-				floodUnits = asyncContext.unitsFormatter('level', floodUnits);
-				floodValue = asyncContext.valueFormatter('level', floodValue);
-				
-				let apiData = "" + floodValue /*+ " " + floodUnits*/;
-				
-				// TODO: update the cached data
-				setterFunc(apiData);
-			}//end-success
-		});//end-$.ajax
-		
-	},//end-getWaterLevel
-	
-};
 
-// EOF
+class NOAAWeatherWater extends APIClient {
+
+	constructor() {
+		super('https://water.weather.gov/ahps2/hydrograph_to_xml.php')
+		this.id = APIClientIdentifier.NOAA_WATER
+	}
+	dataTransformers = {
+		[DatapointIdentifier.WATER_FLOW]: (v) => v,
+		[DatapointIdentifier.WATER_LEVEL]: (v) => v,
+	}
+
+	dataUnits = {
+		[DatapointIdentifier.WATER_FLOW]: 'kcfs',
+		[DatapointIdentifier.WATER_LEVEL]: "ft",
+	}
+
+
+	async _getDatapoint(apiId, parameters = {}, path = "", start_datestamp = undefined, end_datestamp = undefined) {
+		let params = {
+			output: 'xml',		// switch to JSON? -- unsupported yet
+			gage: apiId
+		}
+
+		//combine input parameters with defaults
+		params = Object.assign({}, params, parameters);
+
+		return super.request(path, params)
+	}
+
+	async getDatapoint(datapointId, apiId) {
+		//TODO: check cache
+		switch (datapointId) {
+			case DatapointIdentifier.WATER_FLOW:
+				return this._getDatapoint(apiId)
+					.then(async (response) => {
+						var data = await response.text()
+						// get from data -- XPaths?
+						var datum = $(data).find('observed > datum:first');
+						// var flowUnits = $(datum).find('secondary').attr('units');
+						var flowValue = $(datum).find('secondary').text();
+
+						return this.dataTransformers[datapointId](flowValue);
+					});
+			case DatapointIdentifier.WATER_LEVEL:
+				return this._getDatapoint(apiId)
+					.then(async (response) => {
+						var data = await response.text()
+						// get from data
+						var datum = $(data).find('observed > datum:first');
+						// var floodUnits = $(datum).find('primary').attr('units');
+						var floodValue = $(datum).find('primary').text();
+						return this.dataTransformers[datapointId](floodValue);
+					});
+			default:
+				console.log("datapoint " + datapointId + " not supported by client " + this.id);
+		}
+	}
+
+	getUnits(datapointId) {
+		return dataUnits[datapointId]
+	}
+
+	supportedDatapoints() {
+		return this.dataTransformers.keys()
+	}
+}
+
+
+var gov_weather_water = NOAAWeatherWater();
