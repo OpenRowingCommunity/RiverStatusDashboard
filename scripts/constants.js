84076ec7917cf5d41a4e28a5afc79a364c05f420
Adrian Edwards <adrian@adriancedwards.com>
2023-09-01 11:08:23 -0400

    create a constants file for enum values

---
+++ scripts/constants.js
@@ -0,0 +1,11 @@
+
+const DatapointIdentifier = Object.freeze({
+    WATER_FLOW:		Symbol("waterflow"),
+	WATER_TEMP:		Symbol("watertemp"),
+	WATER_LEVEL:	Symbol("waterlevel"),
+	AIR_TEMP:		Symbol("airtemp"),
+	AIR_SPEED:		Symbol("airspeed"),
+	AIR_DIRECTION:	Symbol("airdirection"),
+	AIR_QUALITY:	Symbol("airquality")
+});
+
