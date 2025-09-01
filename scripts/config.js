e157b3d6f3a028443e136ebc544d4fe6727ab1e4
Adrian Edwards <adrian@adriancedwards.com>
2025-09-01 19:37:22 -0400

    specify rowing matrix for TRRA

--- scripts/config.js
+++ scripts/config.js
@@ -507,7 +507,7 @@ let trraconfig = new RiverStatusConfig({
 	boathouseLat: 40.466846,
 	boathouseLong: -79.976543,
 	safetyMatrix: new SafetyMatrix({
-		version: "2024: Revised May 2024 by TRRA Safety Committee and accepted by TRRA Board",
+		version: "2024 Rowing Matrix: Revised May 2024 by TRRA Safety Committee and accepted by TRRA Board",
 		source: "https://www.threeriversrowing.org/_files/ugd/e46300_61356ec9d051465e9e9bd249ff77e3b4.pdf",
 		safetyZones: [
 			new SafetyZone({
