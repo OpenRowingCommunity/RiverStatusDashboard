715087c5d4a8aec2dbb74d4e24a2b3b98c42020f
Adrian Edwards <adrian@adriancedwards.com>
2026-03-25 20:21:15 -0400

    tell vite about the ~ shortcut too

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      // This maps "~/..." to the "scripts/..." folder
+      '~': path.resolve(__dirname, './scripts'),
+    },
+  },
   test: {
     // You can add more Vitest-specific options here if needed
     globals: true, // This makes `describe`, `it`, `expect` available globally
